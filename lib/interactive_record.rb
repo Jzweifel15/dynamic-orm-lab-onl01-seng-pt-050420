14db6c7e357e2abaaa2b97dad97a465946fa90e0
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-07 19:21:17 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -29,8 +29,9 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
   
-  def self.find_by(row)
-    sql = "SELECT #{row} FROM #{self.table_name}"
+  def self.find_by(attribute)
+    sql = "SELECT #{attribute} FROM #{self.table_name}"
+    DB[:conn].execute(sql)
   end
   
   def initialize(options = {})
